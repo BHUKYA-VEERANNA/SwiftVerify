9a22e218829d0176542ba829a4a880b012f11356
BHUKYA-VEERANNA <bveeranna567@gmail.com>
2024-03-02 19:40:21 +0530

    update for review-2

---
+++ Webpage/public/face_script.js
@@ -0,0 +1,47 @@
+const video = document.getElementById("video");
+
+// Ensure that the paths to the models are correct relative to your HTML file on the server
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models")
+]).then(webCam);
+
+function webCam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+
+  setInterval(async () => {
+    const detection = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+
+    const resizedWindow = faceapi.resizeResults(detection, {
+      height: video.height,
+      width: video.width,
+    });
+
+    faceapi.draw.drawDetections(canvas, resizedWindow);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedWindow);
+
+    console.log(detection);
+  }, 100);
+});
